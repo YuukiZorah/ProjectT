ac2a8816cafcca886ad7ea44ba0d18471e8210f2
YuukiZorah <7252244+YuukiZorah@users.noreply.github.com>
2024-02-05 21:42:47 +0100

    small QOL and usability improvements
    Added screen clearing and wait times, to make it feel more polished.

--- src/Game.cs
+++ src/Game.cs
@@ -2,46 +2,52 @@
 {
     internal class Game
     {
-        private enum GameState { MainMenu, Exit };
+        private enum GameState { Exit, Intro, MainMenu };
         private GameState gameState = GameState.MainMenu;
         private string userInput ="";
-
+        private readonly int waitTime = 1000;
+ 
         public void Run()
         {
-            DisplayMainMenu();
-
-            while (gameState != GameState.Exit)
+            while (gameState == GameState.MainMenu)
             {
-                MainMenu();
+                Console.Clear();
+                Console.WriteLine("Welcome to ProjectT. \n");
+                Console.WriteLine("1. New game");
+                Console.WriteLine("2. Quit");
+                Console.WriteLine("");
+
+                userInput = Console.ReadLine()!;
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Starting a new game.");
+                        gameState = GameState.Intro;
+                        userInput = "";
+                        Thread.Sleep(waitTime);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Quitting game.");
+                        gameState = GameState.Exit;
+                        Thread.Sleep(waitTime);
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter a valid option");
+                        userInput = "";
+                        Thread.Sleep(waitTime);
+                        break;
+                }
             }
-        }
 
-        public static void DisplayMainMenu()
-        {
-            Console.WriteLine("Welcome to ProjectT. \n");
-            Console.WriteLine("1. New game");
-            Console.WriteLine("2. Quit");
-            Console.WriteLine("");
-        }
-
-        public void MainMenu()
-        {
-            userInput = Console.ReadLine()!;
-
-            switch (userInput)
-            {
-                case "1":
-                    Console.WriteLine("Starting a new game.");
-                    break;
-
-                case "2":
-                    Console.WriteLine("Quitting game.");
-                    gameState = GameState.Exit;
-                    break;
-
-                default:
-                    Console.WriteLine("Please enter a valid option");
-                    break;
+            while (gameState == GameState.Intro)
+            { 
+                Console.Clear();
+                Console.WriteLine("It is a warm and sunny day, and you arrive at Hakone in good spirits. It is finally time for you to join the fight against the wretched Xenos.");
+                Console.WriteLine("But before that a long and ardeous training regimen to become an MCU pilot awaits.");
+                userInput = Console.ReadLine()!;
             }
         }
     }
